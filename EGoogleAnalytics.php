01c7aba227fdf57f775690e621cdd9636ee735b0
Ianaré Sévi <ianare@gmail.com>
2012-11-21 14:47:40 +0100

    initial public version

---
+++ EGoogleAnalytics.php
@@ -0,0 +1,183 @@
+<?php
+
+/**
+ * Google Analytics code generation widget
+ *
+ * @copyright © Digitick <www.digitick.net> 2012
+ * @license GNU Lesser General Public License v3.0
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Lesser Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/**
+ * Google Analytics widget class.
+ *
+ * @author Ianaré Sévi
+ */
+class EGoogleAnalytics extends CWidget
+{
+	/**
+	 * @var string The full web property ID (e.g. UA-65432-1) for the tracker object.
+	 */
+	public $account;
+
+	/**
+	 * @var boolean linker functionality flag as part of enabling cross-domain
+	 * user tracking. 
+	 */
+	public $allowLinker = true;
+
+	/**
+	 * @var string the new cookie path for your site. By default, Google Analytics
+	 * sets the cookie path to the root level (/).
+	 */
+	public $cookiePath;
+
+	/**
+	 * @var string the domain name for the GATC cookies. Values: 'auto', 'none', [domain].
+	 * [domain] is a specific domain, i.e. 'example.com'.
+	 * 'auto' by default.
+	 */
+	public $domainName = 'auto';
+
+	/**
+	 * @var array strings of ignored term(s) for Keywords reports.
+	 * 
+	 * array('keyword1', 'keyword2')
+	 */
+	public $ignoredOrganics = array();
+
+	/**
+	 * @var array Excludes a source as a referring site.
+	 * 
+	 * array('referrer1', 'referrer2')
+	 */
+	public $ignoredRefs = array();
+
+	/**
+	 * @var array Adds a search engine to be included as a potential search
+	 * engine traffic source.
+	 * 
+	 * array('engine1' => 'keyword2', 'engine2' => 'keyword2')
+	 */
+	public $organics = array();
+
+	/**
+	 * @var boolean the browser tracking module flag.
+	 */
+	public $clientInfo = true;
+
+	/**
+	 *
+	 * @var boolean the Flash detection flag.
+	 */
+	public $detectFlash = true;
+
+	/**
+	 *
+	 * @var boolean the title detection flag.
+	 */
+	public $detectTitle = true;
+
+	/**
+	 * @var array items purchased, multidimensional.
+	 * 
+	 * array(
+	 * 		array(
+	 * 			'orderId' => '1234',
+	 * 			'sku' => 'DD44',
+	 * 			'name' => 'T-Shirt',
+	 * 			'category' => 'Olive Medium',
+	 * 			'price' => '11.99',
+	 * 			'quantity' => '1'
+	 * 		),
+	 * )
+	 */
+	public $items = array();
+
+	/**
+	 * @var array commercial transactions, multidimensional.
+	 * 
+	 * array(
+	 * 		array(
+	 * 			'orderId' => '1234',
+	 * 			'affiliation' => 'Womens Apparel',
+	 * 			'total' => '28.28',
+	 * 			'tax' => '1.29',
+	 * 			'shipping' => '15.00',
+	 * 			'city' => 'San Jose',
+	 * 			'state' => 'California',
+	 * 			'country' => 'USA'
+	 * 		),
+	 * )
+	 */
+	public $transactions = array();
+
+	public function run()
+	{
+		if (!$this->account)
+			throw new CException('Google analytics account ID must be set.');
+
+		$ignoredOrganics = '';
+		foreach ($this->ignoredOrganics as $keyword) {
+			$ignoredOrganics .= "_gaq.push(['_addIgnoredOrganic', '$keyword']);\n";
+		}
+		$ignoredRefs = '';
+		foreach ($this->ignoredRefs as $referrer) {
+			$ignoredRefs .= "_gaq.push(['_addIgnoredRef', '$referrer']);\n";
+		}
+		$organics = '';
+		foreach ($this->organics as $engine => $keyword) {
+			$organics .= "_gaq.push(['_addOrganic','$engine', '$keyword']);\n";
+		}
+		$items = '';
+		foreach ($this->items as $item) {
+			$items .= "_gaq.push(['_addTrans', '{$item['orderId']}', '{$item['sku']}', '{$item['name']}', '{$item['category']}', '{$item['price']}', '{$item['quantity']}']);";
+		}
+		$transactions = '';
+		foreach ($this->transactions as $trans) {
+			$transactions .= "_gaq.push(['_addTrans', '{$trans['orderId']}', '{$trans['affiliation']}', '{$trans['total']}', '{$trans['tax']}', '{$trans['shipping']}', '{$trans['city']}', '{$trans['state']}', '{$trans['country']}']);";
+		}
+		$trackTrans = '';
+		if (!empty($items) || !empty($transactions))
+			$trackTrans = "_gaq.push(['_trackTrans']);\n";
+
+		$script = "var _gaq = _gaq || [];\n";
+		$script .= "_gaq.push(['_setAccount', '{$this->account}']);\n";
+		$script .= $ignoredOrganics . $ignoredRefs . $organics;
+		$script .= "_gaq.push(['_setDomainName', '{$this->domainName}']);\n";
+		if ($this->cookiePath)
+			$script .= "_gaq.push(['_setCookiePath', '{$this->cookiePath}']);\n";
+		$script .= "_gaq.push(['_setAllowLinker', " . (($this->allowLinker) ? 'true' : 'false') . "]);\n";
+		if (!$this->clientInfo)
+			$script .= "_gaq.push(['_setClientInfo', false]);\n";
+		if (!$this->detectFlash)
+			$script .= "_gaq.push(['_setDetectFlash', false]);\n";
+		if (!$this->detectTitle)
+			$script .= "_gaq.push(['_setDetectTitle', false]);\n";
+		$script .= "_gaq.push(['_trackPageview']);\n";
+		$script .= $items . $transactions . $trackTrans;
+		$script .= "(function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();";
+
+		Yii::app()->getClientScript()->registerScript('google-analytics', $script, CClientScript::POS_HEAD
+		);
+	}
+
+}
+
--- README.md
+++ README.md
@@ -1,4 +1,142 @@
 yii-google-analytics
 ====================
 
-Google Analytics code generation widget for Yii.
+Google Analytics code generation widget for Yii. This extension is designed to include all options which can be logically set from PHP.
+
+##Requirements
+
+Developed and tested on Yii 1.1.12. Should work on all 1.1.x branch.
+
+##Installation
+####Normal
+Download and extract the tarball to your extensions folder.
+
+####Git Submodule
+Alternatively, you may checkout the project as a submodule in Git.
+This will allow you to update to the latest version right in your Git-enabled project.
+[More on Git submodules.](http://git-scm.com/book/en/Git-Tools-Submodules "More on Git submodules.")
+~~~
+$ git submodule add git@github.com:digitick/yii-google-analytics.git protected/extensions/google-analytics
+$ git submodule init
+$ git submodule update
+~~~
+
+##Usage
+
+Add the widget in your main layout, this will make it show up on all pages.
+
+Widget option descriptions have been divided into sections for better legibility, but all options can be mixed.
+The only requirement is for the account to be set.
+
+
+####Basic
+~~~
+[php]
+$this->widget('ext.google-analytics.EGoogleAnalytics', array(
+   'account' => 'UA-XXXXX-X',
+));
+~~~
+
+
+####Domains & Directories
+~~~
+[php]
+$this->widget('ext.google-analytics.EGoogleAnalytics', array(
+   'account' => 'UA-XXXXX-X',
+   'domainName' => 'example.com',
+   'cookiePath' => '/',
+   'allowLinker' => false,
+));
+~~~
+
+
+####Search Engines & Referrers
+~~~
+[php]
+$this->widget('ext.google-analytics.EGoogleAnalytics', array(
+   'account' => 'UA-XXXXX-X',
+   'ignoredOrganics' => array(
+      'www.mydomainname.com',
+   ),
+   'ignoredRefs' => array(
+      'www.sister-site.com',
+   ),
+   'organics' => array(
+      'some-search-engine.com' => 'q',
+   ),
+));
+~~~
+
+
+####E-commerce
+~~~
+[php]
+$this->widget('ext.google-analytics.EGoogleAnalytics', array(
+   'account' => 'UA-XXXXX-X',
+   'items' => array(
+      array(
+         'orderId' => '1234',
+         'sku' => 'DD44',
+         'name' => 'T-Shirt',
+         'category' => 'Olive Medium',
+         'price' => '11.99',
+         'quantity' => '1'
+      ),
+      array(
+         'orderId' => '1234',
+         'sku' => 'DD45',
+         'name' => 'T-Shirt',
+         'category' => 'Black Medium',
+         'price' => '10.99',
+         'quantity' => '2'
+      ),
+   ),
+   'transactions' => array(
+      array(
+         'orderId' => '1234',
+         'affiliation' => 'women-clothes',
+         'total' => '22.98',
+         'tax' => '0.00',
+         'shipping' => '2.58',
+         'city' => 'Miami',
+         'state' => 'FL',
+         'country' => 'USA',
+      ),
+   ),
+));
+~~~
+
+###Global Options
+You can use the Yii configuration file to set options globally throught your application.
+~~~
+[php]
+return array(
+    'components' => array(
+        'widgetFactory' => array(
+            'widgets' => array(
+                'EGoogleAnalytics' => array(
+                    'account' => 'UA-XXXXX-X',
+                    'domainName' => 'example.com',
+                ),
+            ),
+        ),
+    ),
+);
+~~~
+
+##Resources
+
+* [Google Analytics Tracking Basics](https://developers.google.com/analytics/devguides/collection/gajs "Google Analytics Tracking Basics") Official documentation
+* [Github](https://github.com/digitick/yii-google-analytics) Fork it!
+
+
+
+
+
+
+
+
+
+
+
+
